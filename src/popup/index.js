497226bc697b3d2b1ba4f64666328b359ed873cf
u4bi <u4bi@u4bi.com>
2017-09-02 03:25:42 +0900

    save animation render

--- src/popup/index.js
+++ src/popup/index.js
@@ -1,8 +1,9 @@
 onload = () => { 
 
-    const timer = document.getElementById('timer'),
-          content  = document.getElementById('content'),
-          save  = document.querySelector('button');
+    const timer   = document.getElementById('timer'),
+          content = document.getElementById('content'),
+          save    = document.querySelector('button'),
+          notice  = document.querySelector('h3');
 
     console.log(timer, content, save);
 
@@ -24,7 +25,11 @@ onload = () => {
         localStorage.setItem('WORKOUT_CONTENT',  content.value);
         
         chrome.storage.sync.set({'WORKOUT_TIMER': timer.value });
-        chrome.storage.sync.set({'WORKOUT_CONTENT': content.value });
+        chrome.storage.sync.set({'WORKOUT_CONTENT': content.value });         
+        
+        notice.classList.toggle('notice', false);
+        notice.offsetWidth;        
+        notice.classList.toggle('notice', true);
 
     } 
 
